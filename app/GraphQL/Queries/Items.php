5bdc1951818576d4637fb1764fffc66d68714b83
k-700 <Ksuha117744@mail.ru>
2022-04-03 02:05:05 +0500

    Подключение Laravel Ligthouse + схема для GraphQL

---
+++ app/GraphQL/Queries/Items.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+class Items
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+    }
+}
